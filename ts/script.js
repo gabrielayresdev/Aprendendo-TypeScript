356c7252bb812286b928f41927f2378f3b055f8c
Gabriel Ayres <gabriel.ar.fort@gmail.com>
2023-08-24 19:09:24 -0300

    Exercício - Types e Interfaces

--- ts/script.js
+++ ts/script.js
@@ -1,11 +1,18 @@
 "use strict";
-function toNumber(value) {
-    if (typeof value === "number") {
-        return value;
-    }
-    else if (typeof value === "string") {
-        return Number(value);
-    }
-    else
-        throw "value deve ser um número ou uma string";
-}
+const buscaProduto = async () => {
+    const response = await fetch("https://api.origamid.dev/json/notebook.json");
+    const json = await response.json();
+    setData(json);
+};
+buscaProduto();
+const setData = (data) => {
+    document.body.innerHTML = `
+    <div>
+      <h2>${data.nome}</h2>
+      <p>${data.descricao}</p>
+      <br />
+      <span>Fabricante: ${data.empresaFabricante.nome}</span>
+      <span>Fabricante: ${data.empresaMontadora.nome}</span>
+    </div>
+  `;
+};
--- ts/script.ts
+++ ts/script.ts
@@ -1,7 +1,35 @@
-function toNumber(value: number | string) {
-  if (typeof value === "number") {
-    return value;
-  } else if (typeof value === "string") {
-    return Number(value);
-  } else throw "value deve ser um número ou uma string";
+const buscaProduto = async () => {
+  const response = await fetch("https://api.origamid.dev/json/notebook.json");
+  const json = await response.json();
+  setData(json);
+};
+
+buscaProduto();
+
+interface empresa {
+  nome: string;
+  fundacao: number;
+  pais: string;
 }
+
+interface produto {
+  nome: string;
+  preco: number;
+  descricao: string;
+  garantia: string;
+  seguroAcidentes: boolean;
+  empresaFabricante: empresa;
+  empresaMontadora: empresa;
+}
+
+const setData = (data: produto) => {
+  document.body.innerHTML = `
+    <div>
+      <h2>${data.nome}</h2>
+      <p>${data.descricao}</p>
+      <br />
+      <span>Fabricante: ${data.empresaFabricante.nome}</span>
+      <span>Fabricante: ${data.empresaMontadora.nome}</span>
+    </div>
+  `;
+};
