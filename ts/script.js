691b7d5d20f5f320d4bc7d3b1bbf1bdc6133e969
Gabriel Ayres <gabriel.ar.fort@gmail.com>
2023-08-24 18:32:46 -0300

    Exercício - Union Types

--- ts/script.js
+++ ts/script.js
@@ -1,22 +1,11 @@
 "use strict";
-const input = document.querySelector("input");
-const total = localStorage.getItem("total");
-if (input && total) {
-    input.value = total;
-    calcularGanho(Number(input.value));
-}
-function calcularGanho(value) {
-    const p = document.querySelector("p");
-    if (p) {
-        p.innerText = `ganho total: ${value + 100 - value * 0.2}`;
+function toNumber(value) {
+    if (typeof value === "number") {
+        return value;
     }
-}
-function totalMudou() {
-    if (input) {
-        localStorage.setItem("total", input.value);
-        calcularGanho(Number(input.value));
+    else if (typeof value === "string") {
+        return Number(value);
     }
-}
-if (input) {
-    input.addEventListener("keyup", totalMudou);
+    else
+        throw "value deve ser um número ou uma string";
 }
