bae8a1fee8dae55d5bf1400a6444a3aca2534fed
Gabriel Ayres <gabriel.ar.fort@gmail.com>
2023-08-25 22:15:07 -0300

    Exercício - Instanceof

--- ts/script.js
+++ ts/script.js
@@ -1,17 +1,5 @@
 "use strict";
-const buscaCursos = async () => {
-    const response = await fetch("https://api.origamid.dev/json/cursos.json");
-    const json = await response.json();
-    mostraCursos(json);
-};
-buscaCursos();
-const mostraCursos = (cursos) => {
-    const html = cursos.map((curso) => {
-        return `<h1 style="color: ${curso.nivel === "iniciante" ? "red" : "green"};">${curso.nome}</h1>
-    <p>Horas: ${curso.horas}</p>
-    <p>Tipo: ${curso.gratuito ? "gratuito" : "pago"}</p>
-    <p>Tags: ${curso.tags.join(", ")}</p>
-    `;
-    });
-    document.body.innerHTML = `${html}`;
-};
+const link = document.getElementById("origamid");
+if (link instanceof HTMLAnchorElement) {
+    link.href = link.href.replace("http", "https");
+}
--- ts/script.ts
+++ ts/script.ts
@@ -1,31 +1,5 @@
-interface curso {
-  nome: string;
-  horas: number;
-  aulas: number;
-  gratuito: boolean;
-  tags: string[];
-  idAulas: number[];
-  nivel: "iniciante" | "avancado";
+const link = document.getElementById("origamid");
+
+if (link instanceof HTMLAnchorElement) {
+  link.href = link.href.replace("http", "https");
 }
-
-const buscaCursos = async () => {
-  const response = await fetch("https://api.origamid.dev/json/cursos.json");
-  const json = await response.json();
-  mostraCursos(json);
-};
-
-buscaCursos();
-
-const mostraCursos = (cursos: curso[]) => {
-  const html = cursos.map((curso) => {
-    return `<h1 style="color: ${
-      curso.nivel === "iniciante" ? "red" : "green"
-    };">${curso.nome}</h1>
-    <p>Horas: ${curso.horas}</p>
-    <p>Tipo: ${curso.gratuito ? "gratuito" : "pago"}</p>
-    <p>Tags: ${curso.tags.join(", ")}</p>
-    `;
-  });
-
-  document.body.innerHTML = `${html}`;
-};
