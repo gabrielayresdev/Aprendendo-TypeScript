4de5b82af6525c3489b2ee7313c73a5fa0354b1c
Gabriel Ayres <gabriel.ar.fort@gmail.com>
2023-08-26 15:24:51 -0300

    Exercício - User Type Guard

--- ts/script.ts
+++ ts/script.ts
@@ -1,13 +1,52 @@
-function arredondaPraCima(num: number): number;
-function arredondaPraCima(num: string): string;
-function arredondaPraCima(num: number | string) {
-  let result: number | string;
-  result = Math.ceil(Number(num));
-  if (typeof num === "string") {
-    return "" + result;
-  }
-  return result;
+// 1 - Faça um fetch da API: https://api.origamid.dev/json/cursos.json
+// 2 - Defina a interface da API
+// 3 - Crie um Type Guard, que garanta que a API possui nome, horas e tags
+// 4 - Use Type Guards para garantir a Type Safety do código
+// 5 - Preencha os dados da API na tela.
+
+async function getData() {
+  const response = await fetch("https://api.origamid.dev/json/cursos.json");
+  const json = await response.json();
+  handleData(json);
 }
 
-arredondaPraCima("234.5");
-arredondaPraCima(423.34);
+interface Curso {
+  nome: string;
+  horas: number;
+  aulas: number;
+  gratuito: boolean;
+  tags: string[];
+  nivel: "iniciante" | "avancado";
+}
+
+function isCurso(curso: unknown): curso is Curso {
+  if (
+    curso &&
+    typeof curso === "object" &&
+    "nome" in curso &&
+    "horas" in curso &&
+    "aulas" in curso &&
+    "gratuito" in curso &&
+    "tags" in curso
+  ) {
+    return true;
+  }
+  return false;
+}
+
+function handleData(data: unknown) {
+  if (Array.isArray(data)) {
+    data.forEach((curso) => {
+      if (isCurso(curso)) {
+        document.body.innerHTML += `
+        <h1>${curso.nome}</h1>
+        <p>Horas: ${curso.horas}</p>
+        <p>${curso.aulas} aulas</p>
+        <p>${curso.gratuito ? "Gratuito" : "Pago"}</p>
+      `;
+      }
+    });
+  }
+}
+
+getData();
