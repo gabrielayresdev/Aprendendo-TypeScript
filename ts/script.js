e2ed416d545a1015b072a6b17db9d92c107271e3
Gabriel Ayres <gabriel.ar.fort@gmail.com>
2023-08-25 23:17:08 -0300

    Exercício - Eventos e Callback

--- ts/script.js
+++ ts/script.js
@@ -1,5 +1,30 @@
 "use strict";
-const link = document.getElementById("origamid");
-if (link instanceof HTMLAnchorElement) {
-    link.href = link.href.replace("http", "https");
+// Estado dos elementos
+// menu inativo:
+// class="" em nav
+// aria-expanded="false" em button
+// aria-label="Abrir Menu" em button
+// menu ativo:
+// class="active" em nav
+// aria-expanded="true" em button
+// aria-label="Fechar Menu" em button
+const btn = document.querySelector("button");
+function handleClick(event) {
+    const target = event.currentTarget;
+    const nav = document.getElementById("nav");
+    if (target instanceof HTMLElement) {
+        nav?.classList.toggle("active");
+        const active = nav?.classList.contains("active");
+        if (active) {
+            target.setAttribute("aria-expanded", "true");
+            target.setAttribute("aria-label", "Fechar Menu");
+        }
+        else {
+            target.setAttribute("aria-expanded", "false");
+            target.setAttribute("aria-label", "Abrir Menu");
+        }
+    }
 }
+if (btn instanceof HTMLElement)
+    // querySelector retorna um Element, por isso, o evento de pointerdown não pode ser adicionado
+    btn?.addEventListener("pointerdown", handleClick);
--- ts/script.ts
+++ ts/script.ts
@@ -1,5 +1,33 @@
-const link = document.getElementById("origamid");
+// Estado dos elementos
 
-if (link instanceof HTMLAnchorElement) {
-  link.href = link.href.replace("http", "https");
+// menu inativo:
+// class="" em nav
+// aria-expanded="false" em button
+// aria-label="Abrir Menu" em button
+
+// menu ativo:
+// class="active" em nav
+// aria-expanded="true" em button
+// aria-label="Fechar Menu" em button
+
+const btn = document.querySelector("button");
+
+function handleClick(event: PointerEvent) {
+  const target = event.currentTarget;
+  const nav = document.getElementById("nav");
+  if (target instanceof HTMLElement) {
+    nav?.classList.toggle("active");
+    const active = nav?.classList.contains("active");
+    if (active) {
+      target.setAttribute("aria-expanded", "true");
+      target.setAttribute("aria-label", "Fechar Menu");
+    } else {
+      target.setAttribute("aria-expanded", "false");
+      target.setAttribute("aria-label", "Abrir Menu");
+    }
+  }
 }
+
+if (btn instanceof HTMLElement)
+  // querySelector retorna um Element, por isso, o evento de pointerdown não pode ser adicionado
+  btn?.addEventListener("pointerdown", handleClick);
